14e025b67527a2b7a4fcf300da68db75da7ebcc7
isabelontheway <isabelguan1992@gmail.com>
2015-04-18 18:21:15 -0400

    add new scene of maze

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	void Start()
+	{
+		//get the offset between target and camera
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpdate()
+	{
+		//make the camera track the character
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
